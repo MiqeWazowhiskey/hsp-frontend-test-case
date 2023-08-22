382fe2e3a30e85eef0f0a394698f4b48f7ab17e0
Ali Kağan Yılmaz <91954535+MiqeWazowhiskey@users.noreply.github.com>
2023-08-23 01:41:48 +0300

    fix: fallback styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,23 @@ import {createBrowserRouter,RouterProvider} from "react-router-dom";
 import {ErrorPage} from "./Pages/Error";
 import {lazy, Suspense} from "react";
 
-const Dashboard = lazy(() => import('./Pages/Dashboard'));
-const Users = lazy(() => import('./Pages/Users'));
-const Edit = lazy(() => import('./Pages/Edit'));
-const AddUser = lazy(() => import('./Pages/AddUser'));
+const Dashboard = lazy(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return import('./Pages/Dashboard');
+});
+const Users = lazy(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return import('./Pages/Users');
+});
 
+const Edit = lazy(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return import('./Pages/Edit');
+});
+const AddUser = lazy(async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    return import('./Pages/AddUser');
+});
 function App() {
     const router = createBrowserRouter([
         {
@@ -33,7 +45,12 @@ function App() {
     ]);
 
   return (
-              <Suspense fallback={null}>
+              <Suspense fallback={
+                  <div className={'w-screen h-screen suspense-fallback active'}>
+                      <img alt={'logo'} src={'./logo.png'} className={'w-[50%] ml-auto mr-auto mt-[8%]'}/>
+                      <h3 className={'w-full text-center text-3xl font-bold mt-5'}>Loading...</h3>
+                  </div>
+              }>
                   <RouterProvider router={router} />
               </Suspense>
   )
